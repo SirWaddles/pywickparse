56debce9f2dc9f35e75a2bd44303bd9852d589d7
Waddlesworth <github@genj.io>
2019-05-13 16:26:32 +1000

    serializer doesn't show compiler errors

--- src/pyserde.rs
+++ src/pyserde.rs
@@ -1,12 +1,13 @@
 use serde::ser;
 use serde::ser::Serialize;
 use std::fmt;
-use cpython::{PyObject, PyBytes, PyList, Python, ToPyObject, PythonObject};
+use cpython::{PyObject, PyBytes, PyList, PyDict, Python, ToPyObject, PythonObject};
 
 #[derive(Debug)]
 pub enum PyserError {
     SerdeError,
     NotImplemented,
+    Other,
 }
 
 impl fmt::Display for PyserError {
@@ -29,7 +30,8 @@ pub fn to_value<'p, V>(p: Python<'p>, v: V) -> PyserResult<PyObject>
     where V: Serialize + Sized
 {
     let serializer = Serializer {p};
-    Err(PyserError::NotImplemented)
+    let value = v.serialize(serializer)?;
+    Ok(value)
 }
 
 pub struct Serializer<'p> {
@@ -55,7 +57,7 @@ impl<'p> ser::SerializeSeq for ArraySerializer<'p> {
     type Error = PyserError;
 
     fn serialize_element<T>(&mut self, value: &T) -> PyserResult<()>
-        where T: Serialize + Sized
+        where T: ?Sized + Serialize
     {
         let value = to_value(self.p, value)?;
         self.list.insert_item(self.p, self.list.len(self.p), value);
@@ -71,8 +73,144 @@ impl<'p> ser::SerializeTuple for ArraySerializer<'p> {
     type Ok = PyObject;
     type Error = PyserError;
 
-    fn serialize_element<T>(&mut self, value: &T) -> PyserResult<()> {
+    fn serialize_element<T>(&mut self, value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
         ser::SerializeSeq::serialize_element(self, value)
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        ser::SerializeSeq::end(self)
+    }
+}
+
+impl<'p> ser::SerializeTupleStruct for ArraySerializer<'p> {
+    type Ok = PyObject;
+    type Error = PyserError;
+
+    fn serialize_field<T>(&mut self, value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        ser::SerializeSeq::serialize_element(self, value)
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        ser::SerializeSeq::end(self)
+    }
+}
+
+pub struct TupleVariantSerializer {
+
+}
+
+impl ser::SerializeTupleVariant for TupleVariantSerializer {
+    type Ok = PyObject;
+    type Error = PyserError;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        Err(PyserError::NotImplemented)
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        Err(PyserError::NotImplemented)
+    }
+}
+
+pub struct MapSerializer<'p> {
+    p: Python<'p>,
+    keys: Vec<PyObject>,
+    values: Vec<PyObject>,
+}
+
+impl<'p> MapSerializer<'p> {
+    fn new(p: Python<'p>) -> Self {
+        Self {
+            p,
+            keys: Vec::new(),
+            values: Vec::new(),
+        }
+    }
+}
+
+impl<'p> ser::SerializeMap for MapSerializer<'p> {
+    type Ok = PyObject;
+    type Error = PyserError;
+
+    fn serialize_key<T>(&mut self, key: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        let key_obj = to_value(self.p, key)?;
+        self.keys.push(key_obj);
+        Ok(())
+    }
+
+    fn serialize_value<T>(&mut self, value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        let value_obj = to_value(self.p, value)?;
+        self.keys.push(value_obj);
+        Ok(())
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        let dict = PyDict::new(self.p);
+        for i in 0..self.keys.len() {
+            dict.set_item(self.p, &self.keys[i], &self.values[i]).map_err(|_err| PyserError::Other)?;
+        }
+
+        Ok(dict.into_object())
+    }
+}
+
+pub struct StructSerializer<'p> {
+    p: Python<'p>,
+    dict: PyDict,
+}
+
+impl<'p> StructSerializer<'p> {
+    fn new(p: Python<'p>) -> Self {
+        Self {
+            p,
+            dict: PyDict::new(p),
+        }
+    }
+}
+
+impl<'p> ser::SerializeStruct for StructSerializer<'p> {
+    type Ok = PyObject;
+    type Error = PyserError;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        let value = to_value(self.p, value)?;
+        self.dict.set_item(self.p, key, value).map_err(|_err| PyserError::Other)?;
+        Ok(())
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        Ok(self.dict.into_object())
+    }
+}
+
+pub struct StructVariantSerializer {
+
+}
+
+impl ser::SerializeStructVariant for StructVariantSerializer {
+    type Ok = PyObject;
+    type Error = PyserError;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> PyserResult<()>
+        where T: ?Sized + Serialize
+    {
+        Err(PyserError::NotImplemented)
+    }
+
+    fn end(self) -> PyserResult<PyObject> {
+        Err(PyserError::NotImplemented)
     }
 }
 
@@ -81,12 +219,12 @@ impl<'p> ser::Serializer for Serializer<'p> {
     type Error = PyserError;
 
     type SerializeSeq = ArraySerializer<'p>;
-    type SerializeTuple = Self;
-    type SerializeTupleStruct = Self;
-    type SerializeTupleVariant = Self;
-    type SerializeMap = Self;
-    type SerializeStruct = Self;
-    type SerializeStructVariant = Self;
+    type SerializeTuple = ArraySerializer<'p>;
+    type SerializeTupleStruct = ArraySerializer<'p>;
+    type SerializeTupleVariant = TupleVariantSerializer;
+    type SerializeMap = MapSerializer<'p>;
+    type SerializeStruct = StructSerializer<'p>;
+    type SerializeStructVariant = StructVariantSerializer;
 
     fn serialize_bool(self, v: bool) -> PyserResult<PyObject> {
         Ok(v.to_py_object(self.p).into_object())
@@ -148,21 +286,24 @@ impl<'p> ser::Serializer for Serializer<'p> {
     }
 
     fn serialize_none(self) -> PyserResult<PyObject> {
-        Ok(None.to_py_object(self.p).into_object())
+        let val: Option<PyObject> = None;
+        Ok(val.to_py_object(self.p).into_object())
     }
 
     fn serialize_some<T>(self, value: &T) -> PyserResult<PyObject>
-        where T: Serialize + Sized
+        where T: ?Sized + Serialize
     {
         value.serialize(self)
     }
 
     fn serialize_unit(self) -> PyserResult<PyObject> {
-        Ok(None.to_py_object(self.p).into_object())
+        let val: Option<PyObject> = None;
+        Ok(val.to_py_object(self.p).into_object())
     }
 
     fn serialize_unit_struct(self, _name: &'static str) -> PyserResult<PyObject> {
-        Ok(None.to_py_object(self.p).into_object())
+        let val: Option<PyObject> = None;
+        Ok(val.to_py_object(self.p).into_object())
     }
 
     fn serialize_unit_variant(
@@ -189,13 +330,14 @@ impl<'p> ser::Serializer for Serializer<'p> {
         self,
         _name: &'static str,
         _variant_index: u32,
-        variant: &'static str,
-        value: &T,
+        _variant: &'static str,
+        _value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        Ok(None.to_py_object(self.p).into_object())
+        let val: Option<PyObject> = None;
+        Ok(val.to_py_object(self.p).into_object())
 
     }
 
@@ -205,5 +347,35 @@ impl<'p> ser::Serializer for Serializer<'p> {
 
     fn serialize_tuple(self, _len: usize) -> PyserResult<Self::SerializeTuple> {
         Ok(ArraySerializer::new(self.p))
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> PyserResult<Self::SerializeTupleStruct> {
+        Ok(ArraySerializer::new(self.p))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> PyserResult<Self::SerializeTupleVariant> {
+        Err(PyserError::NotImplemented)
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> PyserResult<Self::SerializeMap> {
+        Ok(MapSerializer::new(self.p))
+    }
+
+    fn serialize_struct(self, _name: &'static str, _len: usize) -> PyserResult<Self::SerializeStruct> {
+        Ok(StructSerializer::new(self.p))
+    }
+
+    fn serialize_struct_variant(self, _name: &'static str, _variant_index: u32, _variant: &'static str, _len: usize) -> PyserResult<Self::SerializeStructVariant> {
+        Err(PyserError::NotImplemented)
     }
 }
